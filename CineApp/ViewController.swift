21627074210d7b2d83d542fee869f41355f78130
Alejandro Cruz Paz <alejandrop@backbase.com>
2021-07-19 18:09:44 +0200

    Initial Commit

---
+++ CineApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CineApp
+//
+//  Created by Alejandro Cruz on 19/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
